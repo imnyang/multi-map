23af34cc45e83f4b8149eff4abfdeaeba5084056
imnyang <nyang.nyanglab@gmail.com>
2023-06-09 11:30:50 +0900

    Update index.js

--- src/pages/free-download/index.js
+++ src/pages/free-download/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <a className="button" href="https://firebasestorage.googleapis.com/v0/b/new-world-2ca64.appspot.com/o/%EC%83%88%EB%A1%9C%EC%9A%B4%20%EC%84%B8%EA%B3%84%20(1).zip?alt=media&token=b8cb289b-c661-42aa-b351-96052daaa8fb&_gl=1*nvsqv4*_ga*MTYwMzgyNTUzMi4xNjg1ODc4ODE1*_ga_CW55HF8NVT*MTY4NjI3MzgyNy43LjEuMTY4NjI3MzgzNS4wLjAuMA..">맵 다운로드</a>
+        <a className="button" href="https://firebasestorage.googleapis.com/v0/b/new-world-2ca64.appspot.com/o/%EC%83%88%EB%A1%9C%EC%9A%B4%20%EC%84%B8%EA%B3%84%20(1).zip?alt=media">맵 다운로드</a>
       </div>
     </main>
   )
